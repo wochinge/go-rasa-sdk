640ae4a6152485d141baa2c90b3713a8e690d41b
Tobias Wochinger <t.wochinger@rasa.com>
2021-04-26 00:33:24 +0200

    pass value

--- rasa/tracker_test.go
+++ rasa/tracker_test.go
@@ -21,7 +21,7 @@ func TestSlotsToValidate(t *testing.T) {
 		"candidate":     "interesting value",
 		"candidate_two": "interesting value2",
 	}
-	assert.Equal(t, &expectedCandidates, slotCandidates)
+	assert.Equal(t, expectedCandidates, slotCandidates)
 }
 
 func TestSlotsToValidateWithNoCandidates(t *testing.T) {
@@ -33,5 +33,5 @@ func TestSlotsToValidateWithNoCandidates(t *testing.T) {
 	slotCandidates := tracker.SlotsToValidate()
 
 	expectedCandidates := map[string]interface{}{}
-	assert.Equal(t, &expectedCandidates, slotCandidates)
+	assert.Equal(t, expectedCandidates, slotCandidates)
 }
